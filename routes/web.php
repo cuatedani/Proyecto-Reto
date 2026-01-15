118fbbbdb8869632f3b198b92792c85e9eff953b
cuatedani <cuate.danielito@gmail.com>
2026-01-15 13:15:54 -0700

    Rutas Web

--- routes/web.php
+++ routes/web.php
@@ -7,39 +7,49 @@ use App\Http\Middleware\IsAdmin;
 use App\Http\Middleware\IsUserAuth;
 
 //VISTAS PUBLICAS
+//Vista Pagina Welcome
 Route::get('/', function () {
     return view('welcome');
-});
-Route::get('/login', function () {
-    return view('auth.login');
-});
-Route::get('/register', function () {
-    return view('auth.login');
+})->name('welcome');
+
+Route::middleware('guest')->group(function (){
+    //Vista del Login
+    Route::get('/login', [AuthController::class, 'loginView'])->name('login');
+
+    //Vista del Registro
+    Route::get('/register', [AuthController::class, 'registerView'])->name('register');
+
+    //Vista de Recuperar Contraseña
+    Route::get('/recovery', [AuthController::class, 'recoveryView'])->name('recovery');
 });
 
 //VISTAS DE USUARIO
 Route::middleware([IsUserAuth::class])->group(function() {
+    //Vista del panel
     Route::get('/dashboard', function () {
         return view('dashboard');
-    });
+    })->name('dashboard');
 
-    Route::get('/profile', function () {
-        return view('users.profile');
+    Route::prefix('perfil')->name('perfil.')->group(function() {
+        //Vista de tu perfil
+        Route::get('/', [UserController::class, 'profileView'])->name('ver');
+
+        //Vista de editar perfil
+        Route::get('/editar', [UserController::class, 'editProfileView'])->name('editar');
     });
 });
 
 //VISTAS DE ADMIN
 Route::middleware([IsAdmin::class])->group(function() {
-    Route::get('/users', function () {
-        return view('users.index');
-    });
 
-    Route::get('/users/register', function () {
-        return view('users.register');
-    });
+    Route::prefix('usuarios')->name('usuarios.')->group(function() {
+        //Lista de Usuarios
+        Route::get('/', [UserController::class, 'indexView'])->name('listar');
 
-    Route::get('/users/update', function () {
-        return view('users.update');
-    });
+        //Formulario de Creacion de Usuario
+        Route::get('/agregar', [UserController::class, 'createView'])->name('agregar');
 
+        //Formulario de Edicion de Usuario
+        Route::get('/{id}/editar', [UserController::class, 'editView'])->name('editar');
+    });
 });
