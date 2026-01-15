c845fd2fb0069133a09bcc8df6e18c9b9e67ca5d
cuatedani <cuate.danielito@gmail.com>
2026-01-15 13:15:44 -0700

    Rutas Apis

--- routes/api.php
+++ routes/api.php
@@ -8,27 +8,33 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 //RUTAS PUBLICAS
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
+Route::prefix('auth')->controller(AuthController::class)->group(function() {
+    Route::post('register', 'register');
+    Route::post('login', 'login');
+    Route::post('recovery', 'recovery');
+});
 
 //RUTAS DE USUARIO
 Route::middleware([IsUserAuth::class])->group(function() {
-    Route::controller(AuthController::class)->group(function() {
-        Route::get('getUser', 'getUser');
+    Route::prefix('auth')->controller(AuthController::class)->group(function() {
+        Route::get('user', 'getUser');
         Route::post('logout', 'logout');
     });
 
-    Route::controller(UserController::class)->group(function() {
-        Route::get('show', 'show');
+    Route::prefix('profile')->controller(UserController::class)->group(function() {
+        Route::get('/', 'getProfile');
+        Route::patch('/', 'updateProfile');
+        Route::patch('/password', 'changePassword');
     });
 });
 
 //RUTAS DE ADMIN
 Route::middleware([IsAdmin::class])->group(function() {
-    Route::controller(UserController::class)->group(function() {
-        Route::get('getUser', 'getUser');
-        Route::post('create', 'create');
-        Route::patch('update', 'update');
-        Route::delete('destroy', 'destroy');
+    Route::prefix('users')->controller(UserController::class)->group(function() {
+        Route::get('/', 'getUsers');
+        Route::get('/{id}', 'getUserById');
+        Route::post('/', 'createUser');
+        Route::patch('/{id}', 'updateUser');
+        Route::delete('/{id}', 'deleteUser');
     });
 });
