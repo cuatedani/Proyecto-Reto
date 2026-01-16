d4c5bfffd0f0aaa47c8266c289ad63b77939251a
cuatedani <cuate.danielito@gmail.com>
2026-01-15 18:30:07 -0700

    Quitar Middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Middleware\IsAdmin;
-use App\Http\Middleware\IsUserAuth;
+use App\Http\Middleware\IsUser;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -14,7 +14,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        IsUserAuth::class;
         IsAdmin::class;
     })
     ->withExceptions(function (Exceptions $exceptions): void {
