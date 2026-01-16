6c0514ee3b929f3bef939bcaf6fa7eed55aa7697
cuatedani <cuate.danielito@gmail.com>
2026-01-15 21:20:52 -0700

    Cambio funcionamiento IsUSer

---
+++ app/Http/Middleware/IsUser.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class IsUser
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!Auth::check() || Auth::user()->role !== 'user') {
+            abort(403, 'No autorizado');
+        }
+
+        return $next($request);
+    }
+}
